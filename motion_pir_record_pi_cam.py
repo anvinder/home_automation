bfab68b43f5871c1dae3667a7f8745b2451d92f6
anvinder <anvinder_singh26@yahoo.co.in>
2022-03-10 23:12:33 -0800

    Update motion_pir_record_pi_cam.py

--- motion_pir_record_pi_cam.py
+++ motion_pir_record_pi_cam.py
@@ -4,6 +4,7 @@ from time import sleep, time
 import datetime
 from gpiozero import LED
 from picamera import PiCamera
+from pushbullet import Pushbullet
 camera = PiCamera()
 led = LED(27)
 
@@ -11,6 +12,8 @@ camera.resolution = (400, 200)
 camera.vflip = True
 camera.contrast = 10
 
+pb = Pushbullet("add the access code here")
+# print(pb.devices)
 
 GPIO.setmode(GPIO.BCM)  # Configures pin numbering to Broadcom reference
 
@@ -31,6 +34,8 @@ try:
             folder_name= "/home/pi/Downloads/location_to_mapped_network_folder"
             os.chdir(folder_name)
             file_name =  str(datetime.datetime.now()) + ".h264"
+            dev = pb.get_device('name of device/phone connected to PB')
+            push = dev.push_note("Heading", "SubHeading: Motion Detected")
             camera.start_recording(file_name)
             camera.wait_recording(10)
             sleep(1)
